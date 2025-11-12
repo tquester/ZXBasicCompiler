9a63da7d38be14f15acd75645582b04b407c6dd4
Thomas Quester <thomas@quester-hh.de>
2025-11-12 23:04:30 +0100

    linux jar

---
+++ Artikel2/Assembler/compileRun.sh
@@ -0,0 +1,4 @@
+cd "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler"
+rm "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler//home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler/compiled.tap"
+"c:/sjasmplus-1.20.2.win/sjasmplus.exe" "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler//home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler/main.asm" 2> "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler/log.txt"
+"fuse" "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler//home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler/compiled.tap"
---
+++ Artikel2/Assembler/runemulator.sh
@@ -0,0 +1,2 @@
+cd "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler"
+"fuse" "/home/thomas/workspaces/ZXBasicCompiler/Artikel2/Assembler/basic.tap"
